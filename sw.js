24a5e7ec4e3c7c02ef8577ebcfa2301ba386ef21
Sebastine Ogbuka <sogbuka@Sebastines-MacBook-Pro.local>
2026-03-01 14:21:57 -0800

    Fix paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'save-reminder-v1';
-const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png'];
+const CACHE_NAME = 'save-reminder-v2';
+const ASSETS = ['./', 'index.html', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
