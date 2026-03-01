80c7aef348088564ed1289e25de9f8ecdb1610b4
Sebastine Ogbuka <sogbuka@Sebastines-MacBook-Pro.local>
2026-03-01 14:32:21 -0800

    Fix mobile audio: use WAV file instead of Web Audio API oscillator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'save-reminder-v2';
+const CACHE_NAME = 'save-reminder-v3';
 const ASSETS = ['./', 'index.html', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
